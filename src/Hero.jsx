a6bfaf0396400cb78d14dbd57a3f2a630e515e11
Alfarizy <arifalfrzy@gmail.com>
2026-01-09 18:59:54 +0700

    Fix bug on Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,27 +1,27 @@
 import React from "react";
+import sunsetImg from "../assets/sunset-img.svg";
+import cloudImg from "../assets/cloud-img.svg";
 
 const Hero = () => {
   return (
-    <div>
-      <section>
-        <div class="hero-container">
-          <img
-            class="sunset-img"
-            src="..//images/sunset-img.svg"
-            alt="sunset image"
-          />
-          <img
-            class="cloud-img"
-            src="../images/cloud-img.svg"
-            alt="cloud image"
-          />
-          <div class="title-text">
-            <h1>I'm Alfarizy</h1>
-            <h3>A programmer.</h3>
-          </div>
+    <section>
+      <div className="hero-container">
+        <img
+          className="sunset-img"
+          src={sunsetImg}
+          alt="sunset image"
+        />
+        <img
+          className="cloud-img"
+          src={cloudImg}
+          alt="cloud image"
+        />
+        <div className="title-text">
+          <h1>I'm Alfarizy</h1>
+          <h3>A programmer.</h3>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
